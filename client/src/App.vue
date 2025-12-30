0a999b65ca53a336cb553c6aad485e2ca41a81f4
EspetecDev <manutheking1991@gmail.com>
2025-12-30 09:41:14 +0100

    setup base vue + shadcn

---
+++ client/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Rocket } from 'lucide-vue-next';
+import { Button } from '@/components/ui/button'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-50 text-gray-900 font-sans">
+    <main class="container mx-auto p-4">
+      <h1 class="text-2xl font-bold mb-4">Project Started</h1>
+      <p class="text-gray-600">Edit App.vue to begin.</p>
+      <Rocket class="w-6 h-6 text-blue-500" />
+
+
+      <div class="p-10 flex gap-4">
+        <Button>Default Button</Button>
+        <Button variant="destructive">Delete</Button>
+        <Button variant="outline">Cancel</Button>
+      </div>
+    </main>
+  </div>
+</template>
