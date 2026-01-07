c3e6e70f368ae58926d1f7a8d0cc7a89863ff623
EspetecDev <manutheking1991@gmail.com>
2026-01-07 19:54:35 +0100

    redirect user depending on login status

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import LoginView from '@/views/LoginView.vue';
+import { supabase } from '@/lib/supabase';
+import RegisterView from '@/views/RegisterView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,7 +17,28 @@ const router = createRouter({
         {
             path: '/login', name: 'login', component: LoginView, meta: { hideNavbar: true}
         },
+        {
+            path: '/register', name: 'register', component: RegisterView, meta: { hideNavbar: true}
+        },
     ]
 });
+
+router.beforeEach(async (to, from, next) => { 
+    
+    const {data} = await supabase.auth.getSession();
+    const isLoggedOn = !!data.session;
+
+    console.log(`Navigating to: ${next.name} | Logged in: ${isLoggedOn}`);
+    console.log('session: ', data.session?.user);
+    if (isLoggedOn && to.name === 'login') {
+        return next({path: '/'});
+    }
+
+    if (!isLoggedOn && to.name != 'login') {
+        return next({path:'/login'});
+    }
+
+    next();
+})
 
 export default router;
